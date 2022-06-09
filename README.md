c2124296e77f0e1068dae01918201400382cf7f3
bootsmann1995 <ab@lait.dk>
2022-06-09 13:55:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-starter-9hjp9u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-9hjp9u)
---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ testCount() }}
+  </div>
+</template>
+<script setup>
+let count = 0;
+function testCount() {
+  count = count + 1;
+  console.log(count);
+  return count;
+}
+</script>
